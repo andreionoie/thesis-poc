b0d0c893d9f26e5e3c7c8db8f5389aaa8f4dea5c
Andrei Onoie <andrei.onoie@microfocus.com>
2021-04-26 00:26:24 +0300

    Fix node configs

--- openethereum/run.bat
+++ openethereum/run.bat
@@ -1,2 +1,2 @@
-start "Node 0" /min "C:\Program Files\OpenEthereum\openethereum.exe" --config node0.toml
-start "Node 1" /min "C:\Program Files\OpenEthereum\openethereum.exe" --config node1.toml
+start "Node 0" /min "C:\Program Files\OpenEthereum\openethereum.exe" --config node0.toml --unsafe-expose
+start "Node 1" /min "C:\Program Files\OpenEthereum\openethereum.exe" --config node1.toml --unsafe-expose
